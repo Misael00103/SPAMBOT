6c6f408cf16c6444e980d8a1ca2ecb72e163095c
Misael Beriguete <68718644+Misael00103@users.noreply.github.com>
2022-08-11 22:05:19 -0400

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import pyautogui
 import webbrowser as web
 from time import sleep
 
-web.open('https://web.whatsapp.com/send?phone=8298744467')
+web.open('https://web.whatsapp.com/send?phone=829#####')
 sleep(10)
 
 with open('spam.txt', 'r') as file:
